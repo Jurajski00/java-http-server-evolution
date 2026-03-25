b4984156485186115e0d8e31514f8dfe5238a6ee
Daniel Jurzyk <daniel@Daniels-Macbook-Air.local>
2026-03-25 18:35:38 +0100

    grasping the basics. implemented basic httpserver com.sun.net.httpserver. main class for configuration and startup, mainhandler to process requests and return html response, set up server at port 8080 with default executor

--- serverHTTP/Main.java
+++ serverHTTP/Main.java
@@ -1,7 +1,14 @@
 package serverHTTP;
 
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MainHandler());
+        server.setExecutor(null);
+        server.start();
     }
 }
